26ab0e86d7b50b2006ddbce0c7b24ac9aafb0b70
Benedikt Rötsch <info@benedikt-roetsch.de>
2015-03-06 17:39:28 +0100

    proper drush alias example

--- tests/example.local.alias.drushrc.php
+++ tests/example.local.alias.drushrc.php
@@ -1,8 +1,8 @@
 <?php
-// prod.alias.drushrc.php
+// local.alias.drushrc.php
 $aliases['local'] = array(
   'uri' => 'fancy.dev',
-  'root' => '/var/www/projects/fancy_kickstart',
+  'root' => '/var/www/path/to/your/drupal/installation',
   'remote-host' => 'vagrant',
   'remote-user' => 'vagrant',
 );
